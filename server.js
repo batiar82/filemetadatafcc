cc1ec775c6ab79294f87c7771c690a48602bc869
Glitch (filemetadatafcc) <none>
2017-12-17 21:24:02 +0000

    🐁👰 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,48 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
+
+app.post('/upload', upload.single('avatar'), function (req, res, next) {
+  console.log("entre al upload");
+  console.log(req.file.size);
+  res.json({size: req.file.size});
+})
+
+
+
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
